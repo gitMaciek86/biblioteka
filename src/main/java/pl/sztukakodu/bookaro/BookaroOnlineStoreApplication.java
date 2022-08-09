000a8e75cc25f7b51346935daf708efb3a230ea0
Maciej Grzelec <maciej.and.grzelec@gmail.com>
2022-08-09 23:36:22 +0200

    M2L4 - pierwszy bean

--- src/main/java/pl/sztukakodu/bookaro/BookaroOnlineStoreApplication.java
+++ src/main/java/pl/sztukakodu/bookaro/BookaroOnlineStoreApplication.java
@@ -1,13 +1,29 @@
 package pl.sztukakodu.bookaro;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class BookaroOnlineStoreApplication {
+public class BookaroOnlineStoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookaroOnlineStoreApplication.class, args);
 	}
 
+	private final CatalogService catalogService;
+
+	public BookaroOnlineStoreApplication(CatalogService catalogService) {
+		this.catalogService = catalogService;
+	}
+
+	//CommandLineRunner to interface funkcyjny, ktorego metoda run odpali sie zaraz po postawieniu apki (zainicjalizowaniu kontenera)
+	// np do ladowania danych do apki
+	@Override
+	public void run(String... args) throws Exception {
+		List<Book> bookList = catalogService.findByTitle("Pan Tadeusz");
+		bookList.forEach(System.out::println);
+	}
 }
